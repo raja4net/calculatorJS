e109b9b5c89d295251bc079910ba116255bc2086
Raja Rupinder Singh <raja4net@yahoo.com>
2023-02-24 18:21:21 +0530

    Finalised calculator logic

--- index.js
+++ index.js
@@ -1,71 +1,151 @@
-const numbers = document.querySelectorAll("#number")
-const operators = document.querySelectorAll("#operator")
-const decimal = document.querySelector("#decimal")
-const clear = document.querySelector("#clear")
-const deleteBtn = document.querySelector("#delete")
-const calculation = document.querySelector(".calculation")
-const input = document.querySelector(".input")
+console.clear();
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
 
-let result = 0
-let prevResult = 0
-let displayValue = 0
-let operations = " "
-let btnValue = ""
+const numbers = document.querySelectorAll("#number");
+const operators = document.querySelectorAll("#operator");
+const equals = document.getElementById("equals");
+const decimal = document.querySelector("#decimal");
+const clearScreen = document.querySelector("#clear");
+const deleteBtn = document.querySelector("#delete");
+const lastOperationScreen = document.querySelector(".calculation");
+const currentOperationScreen = document.querySelector(".input");
+let btnValue = "";
 
-input.innerHTML = `${result}`
-calculation.innerHTML = operations
+window.addEventListener("keydown", handleKeyboardInput);
+numbers.forEach((number) => {
+  number.addEventListener("click", () => appendNumber(number.textContent));
+});
 
-function reset () {
-    result = 0
-    prevResult = 0
-    displayValue = 0
-    operations = " "
-    btnValue = ""
-    calculation.innerHTML = operations
-    input.innerHTML = "0"
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => setOperation(operator.textContent));
+});
+
+equals.addEventListener("click", evaluate);
+
+// CLEAR button functionality
+clearScreen.addEventListener("click", clear);
+
+//DELETE button functionality
+deleteBtn.addEventListener("click", deleteFunction);
+
+decimal.addEventListener("click", appendPoint);
+
+function resetScreen() {
+  currentOperationScreen.textContent = "";
+  shouldResetScreen = false;
+}
+
+function clear() {
+  currentOperationScreen.textContent = "0";
+  lastOperationScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
 }
 
 function deleteFunction() {
-    btnValue = btnValue.slice(0,btnValue.length - 1)
-    console.log(btnValue);
-    input.innerHTML = btnValue
-    if (input.innerHTML == "") {
-        input.innerHTML = "0"
-    }
+  btnValue = currentOperationScreen.textContent;
+  btnValue = btnValue.slice(0, btnValue.length - 1);
+  console.log(btnValue);
+  currentOperationScreen.innerHTML = btnValue;
+  if (currentOperationScreen.innerHTML == "") {
+    currentOperationScreen.innerHTML = "0";
+  }
 }
 
-function calculate(e) {
-    console.log(e.target.innerHTML)
-
+function appendPoint() {
+  if (shouldResetScreen) resetScreen();
+  if (currentOperationScreen.textContent === "")
+    currentOperationScreen.textContent = "0";
+  if (currentOperationScreen.textContent.includes(".")) return;
+  currentOperationScreen.textContent += ".";
 }
 
-function btnClicked(e) {
-    let clickedInput  = e.target.innerHTML
-     
-    btnValue = btnValue + clickedInput
-    if (btnValue == 0) {
-        btnValue = 0
-    }
-
-    if(btnValue.length > 1) {
-        if (btnValue[0] === "0") {
-            btnValue = btnValue.slice(1)
-            console.log("Prefix zero removed",btnValue)
-        }
-    }
- input.innerHTML = btnValue
+function add(a, b) {
+  return a + b;
 }
 
-numbers.forEach(number => {
-    number.addEventListener('click', btnClicked)
-})
+function substract(a, b) {
+  return a - b;
+}
 
-operators.forEach(operator => {
-    operator.addEventListener("click", calculate)
-})
+function multiply(a, b) {
+  return a * b;
+}
 
-// CLEAR button functionality
-clear.addEventListener("click", reset)
+function divide(a, b) {
+  return a / b;
+}
 
-//DELETE button functionality
-deleteBtn.addEventListener("click", deleteFunction)
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === "÷" && currentOperationScreen.textContent === "0") {
+    alert("You can't divide by 0!");
+    return;
+  }
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === ".") appendPoint();
+  if (e.key === "=" || e.key === "Enter" || e.keyCode == 13) evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "Escape") clear();
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+    setOperation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === "/") return "÷";
+  if (keyboardOperator === "*") return "×";
+  if (keyboardOperator === "-") return "−";
+  if (keyboardOperator === "+") return "+";
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return substract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
+
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === "0" || shouldResetScreen)
+    resetScreen();
+  if (currentOperationScreen.textContent.length > 14) {
+    return;
+  }
+  currentOperationScreen.textContent += number;
+}
